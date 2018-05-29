91ebcda0e681d8aa49eb32cdca450f8859d6faec
Yiri Wen <yirwen@microsoft.com>
2018-05-29 14:29:56 +0800

    modify partition

--- TARGET_AZ3166/mico_board.c
+++ TARGET_AZ3166/mico_board.c
@@ -259,7 +259,7 @@ const platform_logic_partition_t mico_partitions[] =
         .partition_owner = MICO_FLASH_EMBEDDED,
         .partition_description = "Application",
         .partition_start_addr = 0x0800C000,
-        .partition_length = 0xF4000,   //992k bytes
+        .partition_length = 0xF4000,   //976k bytes
         .partition_options = PAR_OPT_READ_EN | PAR_OPT_WRITE_DIS,
     },
     [MICO_PARTITION_ATE] =
@@ -299,15 +299,15 @@ const platform_logic_partition_t mico_partitions[] =
         .partition_owner = MICO_FLASH_QSPI,
         .partition_description = "OTA Storage",
         .partition_start_addr = 0x70000,
-        .partition_length = 0x98000, //608k bytes
+        .partition_length = 0xF4000, //976k bytes
         .partition_options = PAR_OPT_READ_EN | PAR_OPT_WRITE_EN,
     },
     [MICO_PARTITION_FILESYS] =
     {
         .partition_owner = MICO_FLASH_QSPI,
         .partition_description = "FILESYS",
-        .partition_start_addr = 0x118000,
-        .partition_length = 0x0E8000, //928k bytes
+        .partition_start_addr = 0x164000,
+        .partition_length = 0x09C000, //558k bytes
         .partition_options = PAR_OPT_READ_EN | PAR_OPT_WRITE_EN,
     }
 };
